0e35d4fe18a9d4b8d7be2cedab3083981a84bd1e
max palamarchuk <wingmanxo.max@fourlogic.net>
2020-05-29 04:36:03 +0300

    Add project files.

---
+++ Enlighter.Api/Controllers/PingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Enlighter.Api.Controllers
+{
+    [ApiController]
+    [Route("api/ping")]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> _logger;
+        private static DateTime startupTime = DateTime.UtcNow;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                StartupTime = startupTime,
+                Uptime = (DateTime.UtcNow - startupTime).ToString(),
+                Response = "pong"
+            };
+
+            _logger.LogInformation("pong", result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Enlighter.Api/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Enlighter.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Enlighter.Api.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public ValuesController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string payload)
+        {
+            Console.WriteLine("received a Post: " + payload);
+            _messageService.Enqueue(payload);
+            return Ok("{\"success\": \"true\"}");
+        }
+    }
+}
